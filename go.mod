be17b1f128a512e46648f09872dfda720be12e07
Fanner <lsc-rp@qq.com>
2021-08-28 00:40:23 +0800

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module GoDemo
+
+go 1.16
+
+require github.com/Fannerdev/Hwd v0.0.0-20210827152931-de57e89d46be // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	hwd "github.com/Fannerdev/Hwd"
+)
+
+func main() {
+	fmt.Printf("hwd.GetVersion(): %v\n", hwd.GetVersion())
+}
